53cc535c7a7eb31854a7e607f69637475c40e303
kevin <kevinsignespuigcerver@gmail.com>
2023-11-14 09:17:18 +0100

    eliminado cosas

--- src/main/java/com/kevinsignes/academia/controller/Agente.java
+++ src/main/java/com/kevinsignes/academia/controller/Agente.java
@@ -7,29 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Agente {
 
-    @GetMapping("/")
-    public String indice(){
-        return "pagina";
-    }
-
-    @GetMapping("/asignaturas")
-    public String asignatura(){
-
-        return "asignaturas";
-    }
-
-    @GetMapping("/notas")
-    public String notas(){
-
-        return "notas";
-    }
-
-    @GetMapping("/perfil")
-    public String perfil(){
-
-        return "perfil";
-    }
-
     @GetMapping({"/login"})
     public String goToLogin(){
         return "login";
