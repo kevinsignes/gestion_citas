89045204669f8c4b7b1d6d044df0ef16ebf6d900
kevin <kevinsignespuigcerver@gmail.com>
2023-11-20 16:43:29 +0100

    editar y borrar servicio

--- src/main/java/com/kevinsignes/academia/controller/ServicioController.java
+++ src/main/java/com/kevinsignes/academia/controller/ServicioController.java
@@ -3,13 +3,14 @@ package com.kevinsignes.academia.controller;
 import com.kevinsignes.academia.basededatos.ServicioEntity;
 import com.kevinsignes.academia.repository.IServicioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ServicioController {
@@ -33,4 +34,25 @@ public class ServicioController {
         model.addAttribute("servicio",servicioEntities);
         return "inicio";
     }
+
+    @RequestMapping ("/editar/{id}")
+    public String editarServicio(@PathVariable int id, Model modelo){
+
+        modelo.addAttribute("servicio",
+                iServicioRepository.findById(id).get());
+        return "editar_servicio";
+    }
+
+    @PostMapping ("/actualizar")
+    public String actualizaServicio(@ModelAttribute("servicio") ServicioEntity servicioEntity){
+        iServicioRepository.save(servicioEntity);
+        return "redirect:/inicio";
+    }
+
+    @GetMapping ("/borrar/{id}")
+    public String borrarServicio(@PathVariable int id){
+        iServicioRepository.deleteById(id);
+        return "redirect:/inicio";
+    }
+
 }
