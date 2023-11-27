170c16d472b656d8263195c246c18753358ca19c
kevin <kevinsignespuigcerver@gmail.com>
2023-11-27 21:07:31 +0100

    cambis

--- src/test/java/com/kevinsignes/gestion_citas/GestionCitasApplicationTests.java
+++ src/test/java/com/kevinsignes/gestion_citas/GestionCitasApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AcademiaApplicationTests {
+class GestionCitasApplicationTests {
 
     @Test
     void contextLoads() {
