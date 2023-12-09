c1c8b537b9c6ff182cf9bf807a193fee4f72e99a
kevin <kevinsignespuigcerver@gmail.com>
2023-12-09 17:57:34 +0100

    card

--- src/main/java/com/kevinsignes/gestion_citas/repository/IUserRepository.java
+++ src/main/java/com/kevinsignes/gestion_citas/repository/IUserRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface IUserRepository extends JpaRepository<UserEntity, Integer> {
 
-    //UserEntity findByUser(String usuario);;
-    @Query("SELECT u FROM UserEntity u WHERE u.usuario = ?1")
+    UserEntity findByUsuario(String usuario);;
+   /* @Query("SELECT u FROM UserEntity u WHERE u.usuario = ?1")
     public UserEntity findByUsuario(String usuario);
 
-
+*/
 }
--- src/main/java/com/kevinsignes/gestion_citas/security/SecurityConfig.java
+++ src/main/java/com/kevinsignes/gestion_citas/security/SecurityConfig.java
@@ -6,14 +6,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  {
-
 
     @Bean
     public UserDetailsServiceImpl userDetailsService(){
@@ -46,13 +49,15 @@ public class SecurityConfig  {
                 .formLogin()
                 .usernameParameter("usuario")
                 .loginPage("/login")
-                .defaultSuccessUrl("/inicio") //TODO:
+                .defaultSuccessUrl("/") //TODO:
                 .permitAll()
                 .and()
                 .logout().logoutSuccessUrl("/").permitAll();
 
         return http.build();
     }
+
+
 /*
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
